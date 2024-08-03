2f2a1b2e1660289b977cabd842ef2eafcf2b6ee6
logika <129959872+IvannaDanyliv@users.noreply.github.com>
2024-08-03 10:59:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+let button1 = document.querySelector('.u')
+let button2 = document.querySelector('.uu')
+let button3 = document.querySelector('.uuu')
+let button4 = document.querySelector('.uuuu')
+let plus1 = document.querySelector('.plus1')
+let plus2 = document.querySelector('.plus2')
+let plus3 = document.querySelector('.plus3')
+let plus4 = document.querySelector('.plus4')
+
+
+plus1.style.display ='none'
+plus2.style.display ='none'
+plus3.style.display ='none'
+plus4.style.display ='none'
+
 
